2e253d75b058b466f27f055112b5d733ab646640
hanggarahadianto <hanggarahadianto.@gmail.com>
2025-01-16 09:30:08 +0700

    fix

---
+++ app/internal/layout.tsx
@@ -0,0 +1,92 @@
+// app/internal/layout.tsx
+"use client";
+
+import HeaderNav from "@/src/components/HeaderNav";
+import {
+  AppShell,
+  Burger,
+  Container,
+  NavLink,
+  rem,
+  Stack,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
+import Link from "next/link";
+
+export default function InternalLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const theme = useMantineTheme();
+  const [opened, { toggle }] = useDisclosure();
+  const tablet_match = useMediaQuery("(max-width: 768px)");
+
+  return (
+    <AppShell
+      padding="md"
+      layout="alt"
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+    >
+      <AppShell.Header
+        style={{
+          height: rem(60),
+          boxShadow: tablet_match ? theme.shadows.md : theme.shadows.sm,
+        }}
+      >
+        {/* <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            height: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Text>App Logo</Text>
+        </div> */}
+
+        <Container fluid py="sm" px="lg">
+          <HeaderNav
+          // mobileOpened={mobileOpened}
+          // toggleMobile={toggleMobile}
+          // sidebarState={sidebarState}
+          // onSidebarStateChange={toggleSidebarState}
+          />
+        </Container>
+      </AppShell.Header>
+
+      {/* <AppShell.Navbar p="md">
+        <Stack>
+          <Link
+            href="/internal/sidebar/dashboard"
+            style={{ textDecoration: "none", color: theme.colors.dark[9] }}
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="/internal/sidebar/profile"
+            style={{ textDecoration: "none", color: theme.colors.dark[9] }}
+          >
+            Profile
+          </Link>
+          <Link
+            href="/internal/sidebar/setting"
+            style={{ textDecoration: "none", color: theme.colors.dark[9] }}
+          >
+            Setting
+          </Link>
+        </Stack>
+      </AppShell.Navbar> */}
+
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
---
+++ app/internal/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { BankingPartnerComponent } from "@/src/components/banking-partner/banking-partner";
+import Footer from "@/src/components/landing/footer";
+import { Navbar } from "@/src/components/landing/navbar";
+
+const InternalPage = () => {
+  return (
+    <>
+      <h1>hallo</h1>
+      {/* <Navbar />
+      <BankingPartnerComponent />
+      <Footer /> */}
+    </>
+  );
+};
+
+export default InternalPage;
