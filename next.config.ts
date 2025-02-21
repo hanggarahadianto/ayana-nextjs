ae062c94bf13caa6d50f3b385b9be8e6a96c825e
hanggarahadianto <hanggarahadianto.@gmail.com>
2025-02-21 14:46:36 +0700

    Update Next.js configuration to rewrite API routes and point to Golang backend

--- next.config.ts
+++ next.config.ts
@@ -8,23 +8,11 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL: process.env.URL,
   },
 
-  async headers() {
+  async rewrites() {
     return [
       {
-        // matching all API routes
-        source: "/src/app/utils/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        source: "/api/:path*",
+        destination: "http://134.209.107.103:8080/:path*", // Your Golang backend URL
       },
     ];
   },
