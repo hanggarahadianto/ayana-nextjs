4dbe236969165aad76cdc2572a0c16f048c52a24
hanggarahadianto <hanggarahadianto.@gmail.com>
2025-02-25 11:13:59 +0700

    Refactor FloatingWhatsApp component to use Next.js Link for WhatsApp redirection

--- src/components/product/floating-whatsapp.tsx
+++ src/components/product/floating-whatsapp.tsx
@@ -3,28 +3,15 @@
 import React from "react";
 import { ActionIcon, Stack, Text } from "@mantine/core";
 import { IconBrandWhatsapp } from "@tabler/icons-react";
-import { useMediaQuery } from '@mantine/hooks';
+import { useMediaQuery } from "@mantine/hooks";
+import Link from "next/link";
+
 const FloatingWhatsApp = () => {
-const isMobile = useMediaQuery('(max-width: 768px)');
-const phoneNumber = "62895421711315";
-const message =
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const phoneNumber = "62895421711315";
+  const message =
     "Halo! Saya menghubungi Anda melalui website dan tertarik dengan rumah yang ditawarkan. Bisa berikan informasi lebih lanjut? Jika ada katalog atau daftar harga, saya ingin melihatnya. Apakah ada promo saat ini? Saya tunggu responnya. Terima kasih! 😊";
-const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-const handleClick = () => {
-if (typeof window !== 'undefined') {
-    if (navigator.share) {
-    navigator
-        .share({
-        title: "Hubungi Kami via WhatsApp",
-        text: message,
-        url: whatsappUrl,
-        })
-        .catch((error) => console.log("Error sharing:", error));
-    } else {
-    window.open(whatsappUrl, "_blank");
-    }
-}
-};
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
   return (
     <div
@@ -50,28 +37,29 @@ if (typeof window !== 'undefined') {
         Hubungi Kami
       </Text>
       <Stack align="center" mt={10}>
-        <ActionIcon
-          component="button"
-          onClick={handleClick}
-          style={{
-            background: "linear-gradient(135deg, #25D366, #128C7E)",
-            color: "white",
-            width: isMobile ? "70px" : "90px",
-            height: isMobile ? "70px" : "90px",
-            borderRadius: "50%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            boxShadow: "0px 6px 15px rgba(0, 0, 0, 0.3)",
-            cursor: "pointer",
-            transition: "transform 0.3s ease-in-out",
-            animation: "pulse 1.5s infinite",
-          }}
-          onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.1)")}
-          onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
-        >
-        <IconBrandWhatsapp size={isMobile ? 40 : 60} />
-        </ActionIcon>
+        <Link href={whatsappUrl} target="_blank" rel="noopener noreferrer">
+          <ActionIcon
+            component="button"
+            style={{
+              background: "linear-gradient(135deg, #25D366, #128C7E)",
+              color: "white",
+              width: isMobile ? "70px" : "90px",
+              height: isMobile ? "70px" : "90px",
+              borderRadius: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              boxShadow: "0px 6px 15px rgba(0, 0, 0, 0.3)",
+              cursor: "pointer",
+              transition: "transform 0.3s ease-in-out",
+              animation: "pulse 1.5s infinite",
+            }}
+            onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.1)")}
+            onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
+          >
+            <IconBrandWhatsapp size={isMobile ? 40 : 60} />
+          </ActionIcon>
+        </Link>
       </Stack>
       <style>
         {`
